2895c134251c67a45c860bc3b7d0126a6d257f07
Wu Xiangjiekang <wuxiangyihui@gmail.com>
2022-06-26 08:08:00 +0800

    day2

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,69 @@
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+// // console.log(40 + 8 + 23 - 10);
+// /* */
+// let firstName = 'Jonas';
+// console.log(firstName);
+
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+// console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof 12);
+// console.log(typeof 'Mes');
+
+// javaScriptIsFun = 'YES!';
+// console.log(javaScriptIsFun)
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(year)
+
+// console.log(typeof null);
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// const create variable that cannot be mutated
+// const job; is not valid, must assign value
+
+// lastName = "Xiangjiekang";
+// console.log(lastName);
+// don't do this
+
+const now = 2037;
+const ageJonas = now - 1997;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = "Wu";
+console.log(firstName + ' ' + lastName);
+
+// //Assignment operators
+// let x = 10 + 5;
+// x *= 4;
+// x++;
+// x--;
+// console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge)
+dsads = 0;
