6925db708e67438c34ac99b5aa32be94e6341433
Wu Xiangjiekang <wuxiangyihui@gmail.com>
2022-07-08 02:50:43 +0800

    day7

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,15 +12,18 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '😒 No number!';
+    displayMessage('😒 No number!');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    displayMessage('🎉 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -28,37 +31,38 @@ document.querySelector('.check').addEventListener('click', function () {
     if (score > highScore) {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
-    } else if (guess !== secretNumber) {
-      if (score > 1) {
-        document.querySelector('.message').textContent = '📈 Too high!';
-        score--;
-        document.querySelector('.score').textContent = score;
-      } else {
-        document.querySelector('.message').textContent =
-          '😋 You lose the game!';
-        document.querySelector('.score').textContent = 0;
-      }
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high!';
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '😋 You lose the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '😋 You lose the game!';
+      displayMessage('😋 You lose the game!');
       document.querySelector('.score').textContent = 0;
     }
   }
 });
+// } else if (guess > secretNumber) {
+//   if (score > 1) {
+//     document.querySelector('.message').textContent = '📈 Too high!';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//   } else {
+//     document.querySelector('.message').textContent = '😋 You lose the game!';
+//     document.querySelector('.score').textContent = 0;
+//   }
+// } else if (guess < secretNumber) {
+//   if (score > 1) {
+//     document.querySelector('.message').textContent = '📉 Too low!';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//   } else {
+//     document.querySelector('.message').textContent = '😋 You lose the game!';
+//     document.querySelector('.score').textContent = 0;
+//   }
+// }
+// });
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
@@ -74,13 +78,11 @@ document.querySelector('.again').addEventListener('click', function () {
 ///////////////////////////////////////
 // Coding Challenge #1
 
-/* 
-Implement a game rest functionality, so that the player can make a new guess! Here is how:
+// Implement a game rest functionality, so that the player can make a new guess! Here is how:
 
-1. Select the element with the 'again' class and attach a click event handler
-2. In the handler function, restore initial values of the score and secretNumber variables
-3. Restore the initial conditions of the message, number, score and guess input field
-4. Also restore the original background color (#222) and number width (15rem)
+// 1. Select the element with the 'again' class and attach a click event handler
+// 2. In the handler function, restore initial values of the score and secretNumber variables
+// 3. Restore the initial conditions of the message, number, score and guess input field
+// 4. Also restore the original background color (#222) and number width (15rem)
 
-GOOD LUCK 😀
-*/
+// GOOD LUCK 😀
